1d5fe62cf86767dd9ea269ea5f0d1080140dc46c
domkoch <dominikoch49@gmail.com>
2017-08-04 09:05:12 +0200

    Everything in one place

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>New</h2>
+          <h2>React app</h2>          
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -17,5 +16,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
